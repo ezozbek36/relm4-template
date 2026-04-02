3d770b4d66a0886eae4ef73930ae49c5539ce6f7
Ezozbek <git@ezozbek.dev>
2026-04-02 06:15:16 +0500

    core: refactored python script

--- create-project.py
+++ create-project.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-import platform
-import shutil
 import subprocess
-import sys
-import urllib.request
-import zipfile
+import shutil
 from pathlib import Path
 
+
 print("Welcome to GTK Rust Template")
-while True:
-    name = input("Name (e.g. My Awesome App): ")
-    if len(name) > 0:
-        break
-while True:
-    project_name = input("Project Name (e.g. my-awesome-app): ")
-    if len(project_name) > 0:
-        break
-
-while True:
-    app_id = input(
-        "Application ID (e.g. org.domain.MyAwesomeApp, see: https://developer.gnome.org/documentation/tutorials/application-id.html ): "
-    )
-    if len(app_id) > 0:
-        break
-
-
+name = input("Name (e.g. My Awesome App): ")
+project_name = input("Project Name (e.g. my-awesome-app): ")
+app_id = input(
+    "Application ID (e.g. org.domain.MyAwesomeApp, see: https://developer.gnome.org/documentation/tutorials/application-id.html ): "
+)
 for segment in app_id.split(".")[:-1]:
     if "-" in segment:
         exit("App Id might only contain '-' in the last segment")
@@ -37,13 +21,7 @@ update_contact = input("Email: ")
 project_name_alt = project_name.replace("-", "_")
 
 app_path = "/" + "/".join(app_id.split(".")) + "/"
-current_dir = Path(os.getcwd())
-project_dir = current_dir / project_name
-
-files_url = "https://github.com/Relm4/relm4-template/archive/refs/tags/v0.1.2.zip"
-zip_path = current_dir / "relm4-template.zip"
-template_path = current_dir / "relm4-template-0.1.2"
-online = len(sys.argv) >= 2 and sys.argv[1] == "--online"
+project_dir = Path(__file__).parent / project_name
 
 CURRENT_APP_ID = "com.belmoussaoui.GtkRustTemplate"
 CURRENT_PROJECT_NAME = "gtk-rust-template"
@@ -65,24 +43,6 @@ if project_dir.is_dir():
         shutil.rmtree(project_dir)
     else:
         exit()
-
-if online:
-    zip_destination, _ = urllib.request.urlretrieve(
-        files_url, current_dir / "relm4-template.zip"
-    )
-
-    with zipfile.ZipFile(zip_destination, "r") as zip_ref:
-        zip_ref.extractall(current_dir)
-
-    os.remove(zip_destination)
-
-    for item in os.listdir(template_path):
-        item_path = os.path.join(template_path, item)
-        if "create-project.py" not in item_path:
-            remove_path = os.path.join(current_dir, item)
-            shutil.move(item_path, remove_path)
-
-    shutil.rmtree(template_path)
 
 items_to_copy = [
     Path(".github"),
@@ -154,55 +114,12 @@ files_to_rename = [
 
 for file in files_to_rename:
     current_path = project_dir / file
-    new_path = (
-        project_dir / file.parent / str(file.name).replace(CURRENT_APP_ID, app_id)
-    )
+    new_path = project_dir / file.parent / str(file.name).replace(CURRENT_APP_ID, app_id)
     shutil.move(current_path, new_path)
 
+shutil.move(project_dir / "README_ALT.md", project_dir / "README.md")
 
 subprocess.call(["git", "init"], cwd=project_dir)
 # Add all files and commit them
 subprocess.call(["git", "add", "-A"], cwd=project_dir)
 subprocess.call(["git", "commit", "-m", "Init with GTK Rust Template"], cwd=project_dir)
-
-items_to_delete = [
-    ".flatpak-builder",
-    ".github",
-    "build-aux",
-    "data",
-    "hooks",
-    "po",
-    "src",
-    ".editorconfig",
-    ".gitignore",
-    ".gitlab-ci.yml",
-    "Cargo.lock",
-    "Cargo.toml",
-    "LICENSE.md",
-    "README.md",
-    "create-project.py",
-    "meson.build",
-    "meson_options.txt",
-]
-
-if online:
-    for item in items_to_delete:
-        item_path = os.path.join(current_dir, item)
-        if "create-project.py" not in item_path and project_name not in item_path:
-            remove_path = current_dir / item
-            if remove_path.is_dir():
-                shutil.rmtree(remove_path)
-            else:
-                os.remove(remove_path)
-
-current_os = platform.system()
-
-if current_os == "Windows":
-    subprocess.Popen(f'explorer "{current_dir}"')
-elif current_os == "Darwin":  # macOS
-    subprocess.Popen(["open", current_dir])
-elif current_os == "Linux":
-    subprocess.Popen(["xdg-open", current_dir])
-else:
-    print("Failed to open project path, unsupported operating system.")
-    print("Created project at: ", current_dir)
